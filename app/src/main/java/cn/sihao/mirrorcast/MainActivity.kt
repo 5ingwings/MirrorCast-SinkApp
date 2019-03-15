4ab42840bf46fd39866addd621b3d12adf4a4627
max <563409358@qq.com>
2019-03-15 12:47:40 +0800

    1 设置横屏，添加bg，状态栏透明和隐藏 2 软件处于前台防息屏

--- app/src/main/java/cn/sihao/mirrorcast/MainActivity.kt
+++ app/src/main/java/cn/sihao/mirrorcast/MainActivity.kt
@@ -1,13 +1,26 @@
 package cn.sihao.mirrorcast
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import com.orhanobut.logger.Logger
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val decorView = window.decorView
+        val option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        decorView.systemUiVisibility = option
+        window.statusBarColor = Color.TRANSPARENT
+        // 防息屏
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContentView(R.layout.activity_main)
+
         startMiraCast()
     }
 
@@ -17,11 +30,11 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun startMiraCast(){
+    private fun startMiraCast() {
         MiraMgr.instance.start(applicationContext)
     }
 
-    private fun stopMiraCast(){
+    private fun stopMiraCast() {
         MiraMgr.instance.stop()
     }
 
